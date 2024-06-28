dae610156df21942f3e918dfd8c6bc5049da88f9
Catherine Tony <catherine.tony@tuhh.de>
2024-06-28 12:55:49 +0200

    creating config file for adding filepaths and parameters

---
+++ config.py
@@ -0,0 +1,8 @@
+import yaml
+
+def load_config(config_file='config.yaml'):
+    with open(config_file, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+config = load_config()
